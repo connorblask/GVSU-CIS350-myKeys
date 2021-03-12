9eecacda5cdd11f8955fab34eebbc2fc5bb4643a
Neta Shiff <netashiff1@gmail.com>
2021-03-12 09:00:03 -0500

    files class

--- src/pgp_encryption.py
+++ src/pgp_encryption.py
@@ -17,29 +17,5 @@ class pgp_encryption():
         return self.key
     def encrypt(self):
         ##encrypt using pgp
-
-
-#the class can read and delete files
-import os
-PATH = 'C:'#FIND THE RIGHT PATH
-
-
-class FilesWork:
-    def __init__(self, file_name):
-        self.name_file = file_name
-
-    @staticmethod
-    def delete_file(name_file):
-    ## if file exists, delete it ##
-        if os.path.isfile(PATH + name_file):
-                os.remove(name_file)
-        else:
-         ## Show an error
-                print("Error: %s file not found" % name_file)
-
-    @staticmethod
-    def read_file(name_file):
-        with open(PATH + name_file, 'rb') as files:
-            data = files.read()
-        FilesWork.delete_file(name_file)
-        return data
+        if self.key_type = public_key:
+            return ""
