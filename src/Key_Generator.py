744be9538cc73a1543d9922a5a6f941d46c25a82
Neta Shiff <netashiff1@gmail.com>
2021-04-13 20:28:36 -0400

    check: quit()

--- src/Key_Generator.py
+++ src/Key_Generator.py
@@ -1,13 +1,14 @@
 import os
-
 import gnupg
+from Cryptodome.Cipher import DES3
 from Cryptodome.Random import get_random_bytes
+
 
 class Generation:
     def __init__(self):
         pass
 
-    def pgp_generation(email,pashprase):
+    def pgp_generation(email, pashprase):
         gpg = gnupg.GPG(gnupghome='C:\\Program Files (x86)\\GnuPG\\bin',
                         gpgbinary='C:\\Program Files (x86)\\GnuPG\\bin\\gpg.exe')
 
@@ -19,11 +20,11 @@ class Generation:
         print(key)
         return key
 
-
-    def DES3():
+    def DES3(self):
         key = DES3.adjust_key_parity(get_random_bytes(24))
+        return key
 
-
-    def generate_AES():
+    def generate_AES(self):
         key = os.urandom(16)
         print(key)
+        return key
